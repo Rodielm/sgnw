bb42c0b2f948c7567e0fa14177ab2482736c1f1a
Rodielmj <rodielmj@gmail.com>
2019-08-10 18:33:49 +0200

    added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,23 @@
 
 from fastapi import Depends, FastAPI, Header, HTTPException
 import uvicorn
-
+from starlette.middleware.cors import CORSMiddleware
 from app.api.api import router as api_router
 from app.db.settings import init
+from app.core import config
 
-# from fastapi.security import OAuth2PasswordBearer
-# uvicorn app.main:app --reload
-app = FastAPI()
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
-# @app.get("/items/")
-# async def read_items(token:str = Depends(oauth2_scheme)):
-#     return {"token": token}
+app = FastAPI(config.PROJECT_NAME, openapi_url="/api/v1/openapi.json")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_event_handler("startup", init)
-app.include_router(api_router, prefix="/api")
+app.include_router(api_router, prefix="/api/v1")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
