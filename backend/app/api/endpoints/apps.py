467a9b8933de07558a85b629d667c40d134abdec
Rodielmj <rodielmj@gmail.com>
2019-08-25 16:07:28 +0200

    test upload file backend

--- backend/app/api/endpoints/apps.py
+++ backend/app/api/endpoints/apps.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, Form, UploadFile
 from app.api.models.app import *
 from app.crud import app as db_app
 from fastapi.encoders import jsonable_encoder
 from app.core.utils import create_aliased_response
 from typing import List
+from shutil import copyfile, copyfileobj
+
+import os
+import sys
 
 from starlette.responses import JSONResponse
 from starlette.exceptions import HTTPException
@@ -17,13 +21,14 @@ from starlette.status import (
 
 router = APIRouter()
 
+
 @router.get("/", response_model=List[AppInResponse])
 def read_apps():
     apps = db_app.read_apps()
     return apps
 
 
-@router.get("/{name}",response_model=AppInResponse)
+@router.get("/{name}", response_model=AppInResponse)
 def read_app_by_name(name: str):
     row = db_app.read_app_for_name(name)
     if not row:
@@ -33,14 +38,54 @@ def read_app_by_name(name: str):
     return row
 
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+@router.post("/upload")
+async def create_upload_file(file_upload: UploadFile = File(...)):
+    test_dir = "/home/rodielmj/Desktop/data"
+    # base_dir = os.environ.get('LANGFILE_BASEDIR', None)
+    # dir_list = os.environ.get('LANGFILE_DIRLIST', "")
+    filepath = os.path.join(test_dir, file_upload.filename)
+    print('directory paste: {}'.format(filepath))
+    try:
+        # contents = file_upload.file.read()
+        # print('contents size {}'.format(len(contents)))
+        # with open(filepath, 'wb') as out_file:
+        #     copyfileobj(file_upload.file, out_file)
+        contents = await file_upload.read()
+        with open(contents, 'rb') as f_in, open(filepath, 'wb') as f_out:
+            copyfileobj(f_in, f_out)
+    except Exception as e:
+        print('An exception occurred {}'.format(e))
+    # print('dir_list: {}'.format(dir_list))
+    # if (base_dir is None) or (os.path.isdir(base_dir) is False):
+    #     eprint("LANGFILE_BASEDIR='{}' is not a directory".format(base_dir))
+    return {"filename ": file_upload.filename}
+
+
+@router.post("/files")
+async def create_file(
+    file: bytes = File(...),
+    fileb: UploadFile = File(...),
+    token: str = Form(...)
+):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type
+    }
+
+
 @router.post("/", status_code=HTTP_201_CREATED)
-def create_app(app: AppBase):
+def create_app(app: AppCreate):
     return db_app.create_app(app)
 
 
 @router.put("/{id}")
-def update_app(id:int, app: AppInUpdate):
-    row = db_app.update_app(id,app)
+def update_app(id: int, app: AppInUpdate):
+    row = db_app.update_app(id, app)
     if not row:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND,
                             detail="App not exist",
