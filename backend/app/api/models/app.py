4b33b999d749a21f31462df35341824b3485abe2
Rodielmj <rodielmj@gmail.com>
2019-08-25 16:08:27 +0200

    modified app base with file

--- backend/app/api/models/app.py
+++ backend/app/api/models/app.py
@@ -1,26 +1,42 @@
 
 from typing import (Optional, List)
 from pydantic import BaseModel
-from .master import MasterBase
+from .master import MasterBase, MasterInResponse
+
 
 class AppBase(BaseModel):
     name: str
     description: str = None
     version: str = ''
 
-class AppNotification(BaseModel):
-    id:int
-    name:str = None
-    
-class AppInResponse(AppBase):
-    id:int
+
+class AppLangBase(BaseModel):
+    lang: MasterInResponse
+    version: str
+    filename: str
+
+
+class AppCreate(AppBase):
+    app_langs: List[AppLangBase] = []
+
+
+class AppInResponse(AppCreate):
+    id: int
     pass
+
+
+class AppNotification(BaseModel):
+    id: int
+    name: str = None
+
 
 class AppInDB(AppBase):
     pass
 
+
 class AppInUpdate(AppBase):
     pass
+
 
 class ManyAppsInResponse(AppBase):
     apps: List[AppBase]
